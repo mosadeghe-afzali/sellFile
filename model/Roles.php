de9f8bd9f07fa49ea40ea176b3010aa6022cc266
mosadeghe <mosadegheafzali@yahoo.com>
2021-12-08 18:58:53 +0330

    create a project for sell files

---
+++ model/Roles.php
@@ -0,0 +1,23 @@
+<?php
+include_once 'DB/DB.php';
+;
+
+class Roles
+{
+    const roles = [0 => 'admin', 1 =>'common_user', 2 => 'guest'];
+
+    public function setRoles(){
+        $connection = DB::connect();
+        $statement = $connection->prepare("INSERT INTO roles (role) VALUES (?)");
+        $statement->bind_param('s' , $role);
+        $role= self::roles[1];
+        $statement->execute();
+        $lastRoleId = $connection->insert_id;
+        $connection->close();
+        return $lastRoleId;
+    }
+}
+
+//DELETE FROM my_table
+//WHERE timestamp < NOW() - INTERVAL 30 MINUTE
+//cronjob
